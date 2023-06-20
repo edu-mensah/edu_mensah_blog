3d061d45eb6c77868ed136bb9a6f863f47f1d366
edu-mensah <philipedumensah@gmail.com>
2023-06-20 04:43:04 +0000

    setting up the folder structure

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="system/media/icons/favicon.jpg" type="image/x-icon">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal | Login</title>
+    <!-- custom css -->
+    <link rel="stylesheet" href="system/css/index.css?v=<?= time();?>">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="system/fontawesome/css/all.css?v=<?= time();?>">
+</head>
+<body>
+
+
+
+
+
+
+    <!-- fontawesome -->
+    <script src="system/fontawesome/js/all.js"></script>
+    <!-- custom js -->
+    <script src="system/js/login.js"></script>
+</body>
+</html>
